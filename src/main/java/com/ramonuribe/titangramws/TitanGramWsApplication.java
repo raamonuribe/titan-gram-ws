014a1652065ba61eece69c5347e1dc77dbe411cc
Ramon <ramon@ramonuribe.com>
2021-03-05 14:57:23 -0700

    initialized spring boot application

---
+++ src/main/java/com/ramonuribe/titangramws/TitanGramWsApplication.java
@@ -0,0 +1,13 @@
+package com.ramonuribe.titangramws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanGramWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitanGramWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramonuribe/titangramws/TitanGramWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonuribe.titangramws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanGramWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
