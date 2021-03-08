393974d31cd40a6a0648c389a31890398a23ec83
Ramon <ramon@ramonuribe.com>
2021-03-08 09:29:30 -0700

    Created Repo

---
+++ src/main/java/com/ramonuribe/titangramws/io/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ramonuribe.titangramws.io.repository;
+
+import com.ramonuribe.titangramws.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+}
